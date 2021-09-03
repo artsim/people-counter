9a8fc60d9feed37578885a763cad187feec64647
Arthur Simiyu <hello@arthursimiyu.com>
2021-09-03 22:02:55 +0300

    Define the consumer receive method

--- counter/consumers.py
+++ counter/consumers.py
@@ -1,3 +1,5 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
@@ -11,5 +13,11 @@ class DataConsumer(AsyncWebsocketConsumer):
         await self.disconnect(code)
 
     async def receive(self, text_data):
-        print(text_data)
-        pass
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.groupname, {"type": "processing", "value": data}
+        )
+
+    async def processing(self, event):
+        data = event["value"]
+        await self.send(text_data=json.dumps(data))
